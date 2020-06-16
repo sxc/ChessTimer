5e2579ea38cb3e0a445f9ab2472cf4d6f94fba23
sxc <qiuyequ@icloud.com>
2020-06-16 13:33:39 +0800

    set timer length

--- ChessTimer/ContentView.swift
+++ ChessTimer/ContentView.swift
@@ -10,40 +10,65 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var timerMode: TimerMode = .initial
     @State var selectedPickerIndex = 0
     
+    @ObservedObject var timerManager = TimerManager()
+    
+    
+    
     let availableMinutes = Array(1...59)
+    
     
     var body: some View {
         NavigationView {
             VStack {
-                Text("60")
+                Text("\(timerManager.secondsLeft)")
                     .font(.system(size: 80))
                     .padding(.top, 80.0)
                 
-                Image(systemName: timerMode == .running ? "pause.circle.fill" : "play.circle.fill")
+                Image(systemName: timerManager.timerMode == .running ? "pause.circle.fill" : "play.circle.fill")
                     .resizable()
                     .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                     .frame(width: 180.0, height: 180.0)
                     .foregroundColor(/*@START_MENU_TOKEN@*/.orange/*@END_MENU_TOKEN@*/)
+                    .onTapGesture ( perform: {
+                        
+                        
+                        if self.timerManager.timerMode == .initial {
+                            self.timerManager.setTimerLength(minutes: self.availableMinutes[self.selectedPickerIndex] * 60)
+                            
+                        }
+                        
+                        self.timerManager.timerMode == .running ?
+                            self.timerManager.pause() :
+                            self.timerManager.start()
+                    })
                 
-                if timerMode == .paused {
+                
+                
+                if timerManager.timerMode == .paused {
                     Image(systemName: "gobackward")
                         .resizable()
                         .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
                         .frame(width: 50, height: 50)
                         .padding(.top, 40.0)
+                        .onTapGesture(perform:  {
+                            self.timerManager.reset()
+                        })
                 }
                 
-                if timerMode == .initial {
+                
+                
+                if timerManager.timerMode == .initial {
                     Picker(selection: $selectedPickerIndex, label: Text("")) {
                         ForEach(0 ..< availableMinutes.count) {
                             Text("\(self.availableMinutes[$0]) min")
                         }
                         
+                        
+                        
                     }
-                .labelsHidden()
+                    .labelsHidden()
                 }
                 Spacer()
             }
--- ChessTimer/TimerManager.swift
+++ ChessTimer/TimerManager.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+class TimerManager: ObservableObject {
+    
+    @Published var timerMode: TimerMode = .initial
+    @Published var secondsLeft = UserDefaults.standard.integer(forKey: "timerLength")
+    
+    var timer = Timer()
+    
+    func start() {
+        timerMode = .running
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+            if self.secondsLeft == 0 {
+                self.reset()
+            }
+            self.secondsLeft -= 1
+        })
+    }
+    
+    func reset() {
+        self.timerMode = .initial
+        self.secondsLeft = UserDefaults.standard.integer(forKey: "timerLength")
+        timer.invalidate()
+    }
+    
+    func pause() {
+        self.timerMode = .paused
+        timer.invalidate()
+    }
+    
+    func setTimerLength(minutes: Int) {
+        let defaults = UserDefaults.standard
+        defaults.set(minutes, forKey: "timerLength")
+        secondsLeft = minutes
+    }
+    
+}
